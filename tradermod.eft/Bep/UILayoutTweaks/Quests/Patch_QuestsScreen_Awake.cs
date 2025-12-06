6da6397c570c97376498584e757b1bc3f33f9c04
bmpq <73074311+bmpq@users.noreply.github.com>
2025-12-06 03:04:46 +0300

    quest description layout: dynamic size description

--- tradermod.eft/Bep/UILayoutTweaks/Quests/Patch_QuestsScreen_Awake.cs
+++ tradermod.eft/Bep/UILayoutTweaks/Quests/Patch_QuestsScreen_Awake.cs
@@ -19,6 +19,8 @@ namespace tarkin.tradermod.bep.UI.Quests
         [PatchPostfix]
         private static void PatchPostfix(QuestsListView __instance)
         {
+            Color colorDarkOverlay = new Color(0, 0, 0, 0.8f);
+
             QuestsScreen questsScreen = __instance.GetComponentInParent<QuestsScreen>();
             Transform mainArea = questsScreen.transform.GetChild(0);
             Image bgDark = mainArea.GetComponent<Image>();
@@ -28,8 +30,7 @@ namespace tarkin.tradermod.bep.UI.Quests
             Transform questList = mainArea.GetChild(0).Find("QuestList");
 
             Image newBg = questList.gameObject.AddComponent<Image>();
-            newBg.sprite = bgDark.sprite;
-            newBg.color = bgDark.color;
+            newBg.color = colorDarkOverlay;
 
             RectTransform questViewScrollview = questView.Find("Center/Scrollview") as RectTransform;
             questViewScrollview.offsetMax = new Vector2(0, -60);
@@ -38,18 +39,39 @@ namespace tarkin.tradermod.bep.UI.Quests
             centerBlock.GetComponent<VerticalLayoutGroup>().padding = new RectOffset(0, 0, 15, 15);
 
             Transform descriptionBlock = questView.Find("Center/Scrollview/Content/CenterBlock/DescriptionBlock");
-            descriptionBlock.GetComponent<LayoutElement>().preferredHeight = 500;
+            descriptionBlock.GetComponent<LayoutElement>().enabled = false;
+            SetupVLG(descriptionBlock).padding = new RectOffset(700, 0, 0, 0);
 
             Transform questImage = descriptionBlock.Find("Image");
             questImage.gameObject.SetActive(false);
 
             RectTransform textBlock = descriptionBlock.Find("TextBlock") as RectTransform;
-            textBlock.anchorMin = new Vector2(0, 0);
-            textBlock.anchorMax = new Vector2(1, 1);
-            textBlock.offsetMin = new Vector2(700, 0);
+            SetupVLG(textBlock).padding = new RectOffset(15, 15, 15, 15);
+
+            Image imgTextBlock = textBlock.gameObject.GetComponent<Image>();
+            imgTextBlock.color = colorDarkOverlay;
+            imgTextBlock.gameObject.GetComponent<Mask>().showMaskGraphic = true;
 
             ScrollRectNoDrag scrollRectDescription = textBlock.Find("Scroll").GetComponent<ScrollRectNoDrag>();
-            scrollRectDescription.movementType = ScrollRect.MovementType.Clamped;
+            scrollRectDescription.verticalScrollbar.gameObject.SetActive(false);
+            scrollRectDescription.enabled = false;
+            SetupVLG(scrollRectDescription.RectTransform());
+
+            CustomTextMeshProUGUI textDescription = scrollRectDescription.GetComponentInChildren<CustomTextMeshProUGUI>();
+            textDescription.gameObject.GetComponent<ContentSizeFitter>().enabled = false;
+            textDescription.gameObject.AddComponent<LayoutElement>().minHeight = 360f;
+        }
+
+        static VerticalLayoutGroup SetupVLG(Transform tr)
+        {
+            VerticalLayoutGroup verticalLayoutGroup = tr.gameObject.GetOrAddComponent<VerticalLayoutGroup>();
+
+            verticalLayoutGroup.childForceExpandWidth = false;
+            verticalLayoutGroup.childForceExpandHeight = false;
+            verticalLayoutGroup.childControlHeight = true;
+            verticalLayoutGroup.childControlWidth = true;
+
+            return verticalLayoutGroup;
         }
     }
 }
