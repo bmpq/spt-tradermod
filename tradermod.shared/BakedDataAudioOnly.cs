39f60af087fd3779e485b76a8068b1714e16907f
bmpq <73074311+bmpq@users.noreply.github.com>
2025-12-08 05:05:12 +0300

    fixed mute fence

---
+++ tradermod.shared/BakedDataAudioOnly.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using uLipSync;
+using UnityEngine;
+
+namespace tarkin.tradermod.shared
+{
+    public class BakedDataAudioOnly : BakedData
+    {
+        
+    }
+}
